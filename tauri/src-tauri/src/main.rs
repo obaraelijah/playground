51072e21582455cefcc83763510a561bb1974c70
obaraelijah <elijahobara357@gmail.com>
2024-11-24 13:32:16 +0300

    fix: tauri base setup

--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -4,13 +4,13 @@
   )]
   
   #[tauri::command]
-  fn greet(name: &str) -> String {
+fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
-  }
+}
   
-  fn main() {
+fn main() {
     tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![greet])
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
-  }
+        .plugin(tauri_plugin_shell::init())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
