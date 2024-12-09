592d6686adf6c8682d8f6406f6cf8073273f7091
obaraelijah <elijahobara357@gmail.com>
2024-12-09 12:23:00 +0300

    feat: list projects from projects directory

--- tauri/src/src/lib.rs
+++ tauri/src/src/lib.rs
@@ -1,12 +1,23 @@
-use std::env;
+use anyhow::anyhow;
 
-struct ProjectsDir(pub String);
+use std::env;
+use std::ffi::OsString;
+use std::fs;
+
+#[derive(Debug)]
+struct ProjectsDir(pub OsString);
+
+impl ProjectsDir {
+  fn from_env() -> anyhow::Result<Self> {
+    Ok(Self(OsString::from(env::var("PROJECTS_DIR")?)))
+  }
+}
 
 struct Entry {
-    id: u32,
-    title: String,
-    body: String,
-    published: bool,
+  id: u32,
+  title: String,
+  body: String,
+  published: bool,
 }
 
 #[tauri::command]
@@ -14,10 +25,37 @@ fn greet(name: &str) -> String {
   format!("Hello {}!", name)
 }
 
+fn create_project(name: OsString) -> anyhow::Result<()> {
+  todo!()
+}
+
+fn list_projects(dir: &ProjectsDir) -> anyhow::Result<Vec<OsString>> {
+  let mut projects = Vec::new();
+
+  println!("{:?}", dir);
+
+  for entry in fs::read_dir(&dir.0)? {
+    let file_name = entry?.file_name();
+
+    let file_ext = file_name
+      .to_str()
+      .ok_or(anyhow!("error reading file"))?
+      .split(".")
+      .last()
+      .ok_or(anyhow!("error reading file"))?;
+
+    if file_ext == "db" {
+      projects.push(file_name);
+    }
+  }
+
+  Ok(projects)
+}
+
 pub fn app() -> anyhow::Result<tauri::App<tauri::Wry>> {
   Ok(
     tauri::Builder::default()
-      .manage(ProjectsDir(env::var("PROJECTS_DIR")?))
+      .manage(ProjectsDir::from_env()?)
       .invoke_handler(tauri::generate_handler![greet])
       .build(tauri::generate_context!())?,
   )
@@ -25,7 +63,19 @@ pub fn app() -> anyhow::Result<tauri::App<tauri::Wry>> {
 
 #[cfg(test)]
 mod tests {
-  use super::greet;
+  use std::ffi::OsString;
+
+  use super::{greet, list_projects, ProjectsDir};
+
+  #[test]
+  fn test_list_projects() {
+    dotenv::dotenv().unwrap();
+
+    assert_eq!(
+      list_projects(&ProjectsDir::from_env().unwrap()).unwrap(),
+      Vec::<OsString>::new(),
+    );
+  }
 
   #[test]
   fn test_greet() {
