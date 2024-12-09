7b551117cb2761b928727642f5ac775e8e56eb06
obaraelijah <elijahobara357@gmail.com>
2024-12-09 12:06:34 +0300

    refactor: moved code from main to lib

---
+++ tauri/src/src/lib.rs
@@ -0,0 +1,27 @@
+use std::env;
+
+struct ProjectsDir(pub String);
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+  format!("Hello {}!", name)
+}
+
+pub fn app() -> anyhow::Result<tauri::App<tauri::Wry>> {
+  Ok(
+    tauri::Builder::default()
+      .manage(ProjectsDir(env::var("PROJECTS_DIR")?))
+      .invoke_handler(tauri::generate_handler![greet])
+      .build(tauri::generate_context!())?,
+  )
+}
+
+#[cfg(test)]
+mod tests {
+  use super::greet;
+
+  #[test]
+  fn test_greet() {
+    assert_eq!(greet("World"), "Hello World!".to_owned());
+  }
+}
--- tauri/src/src/main.rs
+++ tauri/src/src/main.rs
@@ -3,31 +3,11 @@
     windows_subsystem = "windows"
   )]
   
-use std::env;
-
-struct ProjectsDir(String);
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello {}!", name)
-}
+use example_app::app;
 
 fn main() -> anyhow::Result<()> {
     dotenv::dotenv()?;
 
-    tauri::Builder::default()
-        .manage(ProjectsDir(env::var("PROJECTS_DIR")?))
-        .plugin(tauri_plugin_shell::init())
-        .run(tauri::generate_context!())?;
+    app()?.run(|_, _| {});
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-  use super::greet;
-
-  #[test]
-  fn test_greet() {
-    assert_eq!(greet("World"), "Hello World!".to_owned());
-  }
 }
